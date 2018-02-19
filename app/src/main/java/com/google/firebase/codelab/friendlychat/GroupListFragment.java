fcb613935c668d519896abe31c650973ca622bfe
noy72 <kuma.tola3@gmail.com>
2018-02-19 15:42:22 +0900

    add fragment

--- app/src/main/java/com/google/firebase/codelab/friendlychat/GroupListFragment.java
+++ app/src/main/java/com/google/firebase/codelab/friendlychat/GroupListFragment.java
@@ -1,11 +1,17 @@
 package com.google.firebase.codelab.friendlychat;
 
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
 
 /**
@@ -13,6 +19,10 @@ import android.view.ViewGroup;
  */
 public class GroupListFragment extends Fragment {
 
+    private View mView; // for findViewById
+    private RecyclerView mRecyclerView;
+    private Button mAddGroupButton;
+    private FragmentManager mFragmentManager;
 
     public GroupListFragment() {
         // Required empty public constructor
@@ -22,8 +32,39 @@ public class GroupListFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_group_list, container, false);
+
+        mView = inflater.inflate(R.layout.fragment_group_list, container, false);
+        return mView;
     }
 
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        mAddGroupButton = mView.findViewById(R.id.addButton);
+        mRecyclerView = mView.findViewById(R.id.groupRecyclerView);
+
+
+        mAddGroupButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                MakeNewGroupDialog dialog = new MakeNewGroupDialog();
+                dialog.setTargetFragment(getParentFragment(), 100);
+                dialog.show(getChildFragmentManager(), "tag");
+            }
+        });
+    }
+
+
+    @Override
+    public void onActivityResult(final int requestCode, final int resultCode, final Intent data) {
+        if (requestCode == 100) {
+            if (resultCode == DialogInterface.BUTTON_POSITIVE) {
+                Toast.makeText(getContext(), "pos", Toast.LENGTH_SHORT).show();
+            } else if (resultCode == DialogInterface.BUTTON_NEGATIVE) {
+                Toast.makeText(getContext(), "neg", Toast.LENGTH_SHORT).show();
+
+            }
+        }
+    }
 }
